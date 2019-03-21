d7debd7efd6926e8af79b2179d45821ab00be0f2
willmcfall <wmcfall2@gmail.com>
2019-03-21 13:19:31 -0400

    fifth commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,93 +13,112 @@ $(document).ready(function () {
         var numIncorrect = 0;
         var userChoice = "";
         var answer = "";
+        var counter = 0;
 
         // present start page and wait for click of start button
-        $("#start").click(function () {
+        $("#start").click(function firstPageFunction() {
 
             // hides the start page and unhides the question page 
             $("#first_page").addClass('d-none');
             $("#second_page").removeClass('d-none');
-
-            // generates 10 rounds of question pages
-            function newPage() {
-
-                // starts a timer that runs for thirty seconds
+            secondPageFunction();
+        });
 
 
-                // randomly draws a question from the larger array question
-                var randQuestion = Math.floor(Math.random() * triviaData.results.length);
-                console.log(triviaData.results.length);
-                console.log(randQuestion);
+        // generates 10 rounds of question pages
+        function secondPageFunction() {
 
-                var question = triviaData.results[randQuestion].question;
-                $(".question").html("Question: " + question);
+            // starts a timer that runs for thirty seconds
 
-                // randomly orders answer options
-                var shuffledArray = triviaData.results[randQuestion].incorrect_answers;
-                shuffledArray.push(triviaData.results[randQuestion].correct_answer);
-                function shuffle(a) {
-                    var j, x, i;
-                    for (i = a.length - 1; i > 0; i--) {
-                        j = Math.floor(Math.random() * (i + 1));
-                        x = a[i];
-                        a[i] = a[j];
-                        a[j] = x;
-                    }
-                    return a;
-                };
-                shuffle(shuffledArray);
-                console.log(shuffledArray);
-                $("#1").html("1: " + shuffledArray[0]);
-                $("#2").html("2: " + shuffledArray[1]);
-                $("#3").html("3: " + shuffledArray[2]);
-                $("#4").html("4: " + shuffledArray[3]);
 
-                // wait for user to select answer option 
-                $(".answer").click(function () {
-                    userChoice = $(this).html();
-                    userChoice = userChoice.replace("1: ", "");
-                    userChoice = userChoice.replace("2: ", "");
-                    userChoice = userChoice.replace("3: ", "");
-                    userChoice = userChoice.replace("4: ", "");
-                    answer = triviaData.results[randQuestion].correct_answer;
-                });
-                
-                // new page function closing tag
-            };
+            // randomly draws a question from the larger array question
+            var randQuestion = Math.floor(Math.random() * triviaData.results.length);
+            console.log(triviaData.results.length);
+            console.log(randQuestion);
 
-            // wait for user to select submit
-            $("#submit").click(function () {
+            var question = triviaData.results[randQuestion].question;
+            $(".question").html("Question: " + question);
 
-                console.log(userChoice);
-                console.log(answer);
-                // check if user selection is the correct answer and count correct responses and incorrect responses
-               
-                if (userChoice == answer) {
-                    alert("Correct!");
-                    userChoice="";
-                    answer="";
-                    number=0;
-                    numCorrect++;
-                    console.log("Number correct = " + numCorrect);
-                    newPage();
+            // randomly orders answer options
+            var shuffledArray = triviaData.results[randQuestion].incorrect_answers;
+            shuffledArray.push(triviaData.results[randQuestion].correct_answer);
+            function shuffle(a) {
+                var j, x, i;
+                for (i = a.length - 1; i > 0; i--) {
+                    j = Math.floor(Math.random() * (i + 1));
+                    x = a[i];
+                    a[i] = a[j];
+                    a[j] = x;
                 }
-                else if (userChoice !== answer) {
+                return a;
+            };
+            shuffle(shuffledArray);
+            console.log(shuffledArray);
+            $("#1").html("1: " + shuffledArray[0]);
+            $("#2").html("2: " + shuffledArray[1]);
+            $("#3").html("3: " + shuffledArray[2]);
+            $("#4").html("4: " + shuffledArray[3]);
 
-                    alert("Incorrect! The correct response is " + answer);
-                    userChoice="";
-                    answer="";
-                    number=0;
-                    numIncorrect++;
-                    console.log("Number incorrect = " + numIncorrect);
-                    newPage();
-                };
-
+            // wait for user to select answer option 
+            $(".answer").click(function () {
+                userChoice = $(this).html();
+                userChoice = userChoice.replace("1: ", "");
+                userChoice = userChoice.replace("2: ", "");
+                userChoice = userChoice.replace("3: ", "");
+                userChoice = userChoice.replace("4: ", "");
+                answer = triviaData.results[randQuestion].correct_answer;
             });
 
-            newPage();
-            // start function closing tag
+            // new page function closing tag
+        };
+
+        // wait for user to select submit
+        $("#submit").click(function () {
+
+            console.log(userChoice);
+            console.log(answer);
+            // check if user selection is the correct answer and count correct responses and incorrect responses
+
+            if (userChoice == answer) {
+                alert("Correct!");
+                userChoice = "";
+                answer = "";
+                numCorrect++;
+                counter++;
+                console.log("Number correct = " + numCorrect);
+                
+            }
+            else if (userChoice !== answer) {
+
+                alert("Incorrect! The correct response is " + answer);
+                userChoice = "";
+                answer = "";
+                numIncorrect++;
+                counter++;
+                console.log("Number incorrect = " + numIncorrect);
+                
+            };
+
+            if(counter < 5){
+                secondPageFunction();
+            }
+            else{
+                thirdPageFunction();  
+            } 
+
         });
+
+
+        function thirdPageFunction() {
+
+            $("#first_page").addClass('d-none');
+            $("#second_page").addClass('d-none');
+            $("#third_page").removeClass('d-none');   
+            $("#close_1").html("Number of Correct Responses: " + numCorrect);
+            $("#close_2").html("NUmber of Incorrect Responses: " + numIncorrect);
+            counter = 0;
+        };
+
 
         // API function closing tag
     });
